b45441f1ceca37186216714f62ec8f44c40c3d7d
bryanhoneck <bryan.honeck@gmail.com>
2021-03-21 21:38:19 -0500

    v 0.97
    Updated some text, flexed the menu for a better look, preparing to add more projects

---
+++ main.js
@@ -0,0 +1,11 @@
+"use strict";
+
+(function(){
+    window.addEventListener("load", init)
+
+    function init(){
+        alert("Welcome to my data science page! Explore the tabs to learn more about me and the projects I have worked on. -Bryan")
+    }
+
+
+})();
